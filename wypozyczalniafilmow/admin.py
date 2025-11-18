0e2b6f61ec07078eb9f6869b8f4fe4dd946de7a8
wozniepsalm <majawozniak71@gmail.com>
2025-11-18 16:24:27 +0100

    dodanie production year w w admin.py

--- wypozyczalniafilmow/admin.py
+++ wypozyczalniafilmow/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Genre, Director, Movie
+from .models import Genre, Director, ProductionYear, Movie
 
 admin.site.register(Genre)
 admin.site.register(Director)
+admin.site.register(ProductionYear)
 admin.site.register(Movie)
 # Register your models here.
