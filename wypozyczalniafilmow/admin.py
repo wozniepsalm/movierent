f1a41b6015131a61890710661a8d648ee4b250e7
wozniepsalm <majawozniak71@gmail.com>
2025-11-19 23:03:15 +0100

    naprawione admin.py zeby serwer działał (brakowało przecinków)

--- wypozyczalniafilmow/admin.py
+++ wypozyczalniafilmow/admin.py
@@ -8,7 +8,7 @@ admin.site.register(Genre, GenreAdmin)
 
 class DirectorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'country')
-    list_filter = ('country')
+    list_filter = ('country',)
     search_fields = ('first_name', 'last_name', 'country')
 admin.site.register(Director, DirectorAdmin)
 
@@ -19,12 +19,12 @@ admin.site.register(ProductionYear, ProductionYearAdmin)
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('title', 'production_year', 'duration_minutes', 'movie_format', 'director', 'genre', 'is_rented')
-    list_filter = ('movie_format', 'production_year', 'director', 'genre', 'is_rented')
+    list_filter = ('movie_format', 'production_year', 'director', 'genre')
     search_fields = ('title', 'director__first_name', 'director__last_name', 'genre__name')
 admin.site.register(Movie, MovieAdmin)
 # Register your models here.
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'plec', 'rented_movie', 'is_renting')
-    list_filter = ('plec', 'is_renting')
+    list_filter = ('plec',)
     search_fields = ('first_name', 'last_name')
 admin.site.register(Customer, CustomerAdmin) 
