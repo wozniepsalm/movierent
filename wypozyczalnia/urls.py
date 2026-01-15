d4a54d0c436e7123b3d5f19c0d9293cf316a4bb1
wozniepsalm <majawozniak71@gmail.com>
2026-01-15 15:03:17 +0100

    dodanie widoku strony głównej i zrobienie geta dla loginu by się wyświetlało w wyszukiwarce

--- wypozyczalnia/urls.py
+++ wypozyczalnia/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from wypozyczalniafilmow.views import welcome_view
 
 urlpatterns = [
+    path('', welcome_view, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('wypozyczalniafilmow.urls')),
 ]
--- wypozyczalniafilmow/urls.py
+++ wypozyczalniafilmow/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from . import views
-from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     
     path('register/', views.UserRegisterView.as_view(), name='register'),
-    path('login/', obtain_auth_token, name='api_token_auth'),
+    path('login/', views.CustomAuthToken.as_view(), name='api_token_auth'),
 
     path('movies/', views.MovieList.as_view(), name='movie-list'),
     path('movies/<int:pk>/', views.MovieDetail.as_view(), name='movie-detail'),
